61ad5dcb704306fb88ed6b3bf7788654ed27797a
Ernei3 <45853073+Ernei3@users.noreply.github.com>
2020-04-08 11:34:09 +0200

    Adding form for adding Categories.

--- app/controllers/CategoryController.scala
+++ app/controllers/CategoryController.scala
@@ -2,36 +2,50 @@ package controllers
 
 import javax.inject._
 import models.CategoryRepository
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
 import scala.concurrent.ExecutionContext
 
 
 class  CategoryController @Inject()(categoryRepo: CategoryRepository, cc: MessagesControllerComponents)(implicit ec: ExecutionContext) extends MessagesAbstractController(cc) {
 
-  def categories = Action.async { implicit request =>
-    val kategorie = categoryRepo.list()
-    kategorie.map( categories => Ok(views.html.categories(categories)))
+  val categoryForm: Form[CreateCategoryForm] = Form {
+    mapping(
+      "name" -> nonEmptyText
+    )(CreateCategoryForm.apply)(CreateCategoryForm.unapply)
   }
 
 
-  def addCategoryMenu = Action {
-  Ok(views.html.index("Menu to add a category"))
+    def categories = Action.async { implicit request =>
+      val kategorie = categoryRepo.list()
+      kategorie.map( categories => Ok(views.html.categories(categories)))
+    }
+
+
+    def addCategoryMenu = Action { implicit request =>
+      Ok(views.html.addCategoryMenu(categoryForm))
+  }
+
+    def addCategory = Action {
+    Ok(views.html.index("Adding a new category"))
+  }
+
+    def getCategory(id: Integer) = Action {
+    Ok(views.html.index("Category: "+id))
+  }
+
+    def updateCategory = Action {
+    Ok(views.html.index("Updating a category"))
+  }
+
+    def removeCategory = Action {
+    Ok(views.html.index("Removing a category"))
+  }
+
 }
 
-  def addCategory = Action {
-  Ok(views.html.index("Adding a new category"))
-}
-
-  def getCategory(id: Integer) = Action {
-  Ok(views.html.index("Category: "+id))
-}
-
-  def updateCategory = Action {
-  Ok(views.html.index("Updating a category"))
-}
-
-  def removeCategory = Action {
-  Ok(views.html.index("Removing a category"))
-}
-
-}
+case class CreateCategoryForm(name: String)

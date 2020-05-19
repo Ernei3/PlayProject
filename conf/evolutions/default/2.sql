952f06b2e047127b7f0460d9b1a67dc51d1467cd
Ernei3 <45853073+Ernei3@users.noreply.github.com>
2020-05-19 10:44:34 +0200

    Commit before branching out.

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -12,10 +12,12 @@ INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("Th
 INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("Words of Radiance", "A book by Brandon Sanderson.", 32, 3);
 INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("Oathbringer", "A book by Brandon Sanderson.", 32, 3);
 INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("blue jeans", "A pair of blue jeans.", 20, 1);
+INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("black jeans", "A pair of black jeans.", 21, 1);
 INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("white shirt", "A white shirt.", 20, 2);
 INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("red shirt", "A red shirt.", 22, 2);
 INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("flower shirt", "A shirt with a flower pattern.", 23, 2);
 INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("The Shadows Between Us", "A book by Tricia Levenseller.", 29, 4);
+INSERT INTO "product"("name", "description", "price", "subcategory") VALUES ("Pride and Prejudice", "A book by Jane Austin.", 22, 4);
 
 INSERT INTO "review"("title", "content", "product") VALUES ("Brilliant!", "Amazing and outstanding!", 1);
 INSERT INTO "review"("title", "content", "product") VALUES ("Best book ever!", "I've never read anything better!", 1);
@@ -33,18 +35,22 @@ INSERT INTO "basket"("user", "quantity", "product") VALUES (3, 2, 5);
 
 
 INSERT INTO "order"("user","status", "address") VALUES (1, "Delivered", 1);
+INSERT INTO "order"("user","status", "address") VALUES (1, "Delivered", 1);
 INSERT INTO "order"("user","status", "address") VALUES (2, "Accepted", 2);
 
 
 INSERT INTO "orderprod"("name","price", "quantity", "order") VALUES ("Twilight", 28, 1, 1);
-INSERT INTO "orderprod"("name","price", "quantity", "order") VALUES ("Roge One", 23, 1, 2);
+INSERT INTO "orderprod"("name","price", "quantity", "order") VALUES ("New Moon", 30, 1, 2);
+INSERT INTO "orderprod"("name","price", "quantity", "order") VALUES ("Host", 31, 1, 2);
+INSERT INTO "orderprod"("name","price", "quantity", "order") VALUES ("Roge One", 23, 1, 3);
 
 INSERT INTO "orderad"("country","city", "street", "number") VALUES ("Poland", "Krakow", "Wielka", "22b");
 INSERT INTO "orderad"("country","city", "street", "number") VALUES ("Poland", "Warsaw", "Lwowska", "21/3");
 
 
 INSERT INTO "payment"("number","name", "date", "code", "order") VALUES ("1111111111111111", "Ala Makota", "2/25", "011", 1);
-INSERT INTO "payment"("number","name", "date", "code", "order") VALUES ("2222222222222222", "Jan Kowalski", "2/25", "222", 2);
+INSERT INTO "payment"("number","name", "date", "code", "order") VALUES ("1111111111111111", "Ala Makota", "2/25", "011", 2);
+INSERT INTO "payment"("number","name", "date", "code", "order") VALUES ("2222222222222222", "Jan Kowalski", "2/25", "222", 3);
 
 # --- !Downs
 
